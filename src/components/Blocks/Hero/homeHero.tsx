f85f491066e5b2e7fc1bcb25cb2472736de3d9b0
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-12-03 11:52:15 +0000

    added the hero content

--- src/components/Blocks/Hero/homeHero.tsx
+++ src/components/Blocks/Hero/homeHero.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 interface IProp {
@@ -20,7 +20,13 @@ const HeroDesktop: FC<IProp> = ({
       position={"relative"}
       overflow={'hidden'}
     >
-      <Box pos={"absolute"} top={0} left={0} bg={"#021821d6"} w={'100%'} h="100%"></Box>
+      <Flex align={"center"} justify={"center"} pos={"absolute"} top={0} left={0} bg={"#021821d6"} w={'100%'} h="100%">
+        <Box w="50%">
+          <Text fontSize={48} textAlign={"center"} fontFamily={"Montserrat"} color={"base.gold"}>
+            Restoring the integrity of the word God in it full glory and life in the body of Christ
+          </Text>
+        </Box>
+      </Flex>
       <Image src="/images/hero-section.jpg" />
     </Box>
   )
