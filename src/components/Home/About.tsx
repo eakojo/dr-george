d64e631878d31b3371afc5a72e2dc8ad983a0b45
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-12-03 16:55:44 +0000

    added about section

---
+++ src/components/Home/About.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+import { Box, Flex, Grid, GridItem, Image, Text } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+
+interface IProp {
+  bgStyle?: any
+}
+
+const About: FC<IProp> = ({
+  bgStyle,
+}) => {
+  return (
+    <Box
+        w="full"
+        minH={'calc(100vh - 250px)'}
+        bg={'#00374d17'}
+        py={24}
+        px={{ base: 4, '2xl': 28 }}
+    >
+        <Text fontSize={40} color={"base.gold"} my={6}> About Dr. George</Text>
+        <Grid templateColumns={"repeat(5, 1fr)"} gap={12}>
+            <Box as={GridItem}  colSpan={2}>
+                <Box w={"100%"} bg="gray.400" h={'100%'}></Box>
+            </Box>
+
+            <Box as={GridItem} colSpan={3}>
+                <Text fontFamily={"montserrat"} color={"base.blue-dark"} fontSize={24}>Bible Teacher and a Conference Speaker</Text>
+                <Text fontFamily={"montserrat"} color={"base.blue-dark"} fontSize={24}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos magnam delectus nesciunt quis quibusdam, nam autem, laborum, facere aliquid corporis voluptatem officiis id minus ipsum nihil optio est consequuntur? Ut, ipsa incidunt. Voluptate, officia qui? Earum dolorum dignissimos illo animi repellat repudiandae natus reprehenderit eaque quidem veniam, nobis labore ullam?</Text>
+            </Box>
+        </Grid>
+    </Box>
+  )
+}
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Layout from '@/components/Layout/Index'
 import HeroDesktop from '@/components/Blocks/Hero/homeHero'
+import About from '@/components/Home/About'
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <HeroDesktop />
+      <About />
     </Layout>
   )
 }
