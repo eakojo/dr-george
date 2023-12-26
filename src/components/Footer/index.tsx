8f9b9614c7000739ace0062a9c69bbaed2279540
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-12-26 11:30:03 +0000

    home responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,28 +7,28 @@ const Footer: FC = () => {
   const router = useRouter()
   return (
     <Box pos={"relative"} zIndex={3}>
-    <Grid templateColumns={'repeat(8, 1fr)'} gap={12} bg={"#262626"} py={20}  px={{ base: 4, '2xl': 36 }}>
-      <GridItem colSpan={3}>
-        <Text color={"white"} fontSize={24} fontWeight={600} mb={4}>About Dr. Arthur</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"}>George Wilfred Arthur Ministries (G. W. Arthur ministries) is an effective ministry with the vision of Restoring the integrity of the word God in its full glory and life in the body of Christ. </Text>
+    <Grid templateColumns={{base: 'repeat(6, 1fr)' , md: 'repeat(8, 1fr)'}} gap={12} bg={"#262626"} py={20}  px={{ base: 4, '2xl': 36 }}>
+      <GridItem colSpan={{base: 6, md: 3}}>
+        <Text color={"white"} fontSize={24} fontWeight={600} mb={4} textAlign={{base: "center", md: "left"}}>About Dr. Arthur</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} textAlign={{base: "center", md: "left"}} fontFamily={"montserrat"}>George Wilfred Arthur Ministries (G. W. Arthur ministries) is an effective ministry with the vision of Restoring the integrity of the word God in its full glory and life in the body of Christ. </Text>
       </GridItem>
-      <GridItem>
+      <GridItem colSpan={{base: 3, md: 2}}>
         <Text color={"white"} fontSize={24} fontWeight={600} mb={4}>Links</Text>
 
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Home</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Biography</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Contact</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Home</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Biography</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Contact</Text>
 
       </GridItem>
-      <GridItem colSpan={2}>
+      <GridItem colSpan={{base: 3, md: 2}}>
         <Text color={"white"} fontSize={24} fontWeight={600} mb={4}>External Links</Text>
 
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Youtube</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Google Podcast</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Apple Podcast</Text>
-        <Text color={"whiteAlpha.600"} fontSize={14} fontFamily={"montserrat"} mb={3}>Spotify</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Youtube</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Google Podcast</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Apple Podcast</Text>
+        <Text color={"whiteAlpha.600"} fontSize={{base: 12, md: 14}} fontFamily={"montserrat"} mb={3}>Spotify</Text>
       </GridItem>
-      <GridItem colSpan={2}>
+      <GridItem colSpan={{base: 6, md: 3}}>
         <Text color={"white"} fontSize={24} fontWeight={600} mb={4}>From The Gallery</Text>
 
         <Grid templateColumns={'repeat(3, 1fr)'} rowGap={6} >
