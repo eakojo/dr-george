b633e2e507e247bfa3e83be12b5a99156b8c816c
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2024-03-03 08:37:48 +0000

    footer update

--- src/internationalization/footer.ts
+++ src/internationalization/footer.ts
@@ -8,30 +8,20 @@ const footerText = {
         footerContact: "Contact",
         footerExternalLinks: "External Links",
         footerGallery: "From The Gallery",
-        footerCopyright: "Copyright © G W Arthur Minitries - 2023"
+        footerCopyright: "Copyright @G. W. Arthur Ministries - 2024"
     },
     fr: {
         footerTitle: "À propos du Dr. Arthur",
-        footerInfo: "George Wilfred Arthur est le berger et la force motrice derrière les Ministères George Wilfred Arthur (G. W. Arthur Ministries). Avec un engagement profond envers la restauration de la Parole de Dieu, le Rév. George apporte à son ministère un mélange unique de passion, de sagesse et d'amour.",
+        footerInfo: "Le Dr. George Wilfred Arthur est le berger et la force motrice derrière les Ministères George Wilfred Arthur (G. W. Arthur Ministries). Avec un engagement profond envers la restauration de la Parole de Dieu, le Rév. George apporte un mélange unique de passion, de sagesse et d'amour à son ministère.",
         footerLinks: "Liens",
         footerHome: "Accueil",
         footerBiography: "Biographie",
         footerContact: "Contact",
-        footerExternalLinks: "Liens externes",
-        footerGallery: "De la galerie",
-        footerCopyright: "Droits d'auteur © Ministères G W Arthur - 2023"
+        footerExternalLinks: "Liens Externes",
+        footerGallery: "De la Galerie",
+        footerCopyright: "Droits d'auteur @G. W. Arthur Ministries - 2024"
     },
-    es: {
-        footerTitle: "Acerca del Dr. Arthur",
-        footerInfo: "George Wilfred Arthur es el pastor y la fuerza impulsora detrás de los Ministerios George Wilfred Arthur (G. W. Arthur Ministries). Con un profundo compromiso con la restauración de la Palabra de Dios, el Rev. George aporta una mezcla única de pasión, sabiduría y amor a su ministerio.",
-        footerLinks: "Enlaces",
-        footerHome: "Inicio",
-        footerBiography: "Biografía",
-        footerContact: "Contacto",
-        footerExternalLinks: "Enlaces externos",
-        footerGallery: "Desde la galería",
-        footerCopyright: "Derechos de autor © Ministerios G W Arthur - 2023"
-    },
+
     pt: {
         footerTitle: "Sobre o Dr. Arthur",
         footerInfo: "George Wilfred Arthur é o pastor e a força motriz por trás dos Ministérios George Wilfred Arthur (G. W. Arthur Ministries). Com um compromisso profundo com a restauração da Palavra de Deus, o Rev. George traz uma mistura única de paixão, sabedoria e amor para o seu ministério.",
@@ -39,22 +29,34 @@ const footerText = {
         footerHome: "Início",
         footerBiography: "Biografia",
         footerContact: "Contato",
-        footerExternalLinks: "Links externos",
+        footerExternalLinks: "Links Externos",
         footerGallery: "Da Galeria",
-        footerCopyright: "Direitos autorais © Ministérios G W Arthur - 2023"
-
+        footerCopyright: "Direitos Autorais @G. W. Arthur Ministries - 2024"
     },
+
+    es: {
+        footerTitle: "Acerca del Dr. Arthur",
+        footerInfo: "George Wilfred Arthur es el pastor y la fuerza impulsora detrás de los Ministerios George Wilfred Arthur (G. W. Arthur Ministries). Con un compromiso profundo con la restauración de la Palabra de Dios, el Rev. George aporta una mezcla única de pasión, sabiduría y amor a su ministerio.",
+        footerLinks: "Enlaces",
+        footerHome: "Inicio",
+        footerBiography: "Biografía",
+        footerContact: "Contacto",
+        footerExternalLinks: "Enlaces Externos",
+        footerGallery: "Desde la Galería",
+        footerCopyright: "Derechos de autor @G. W. Arthur Ministries - 2024"
+    },
+
     ch: {
         footerTitle: "关于亚瑟博士",
-        footerInfo: "乔治·威尔弗雷德·亚瑟是乔治·威尔弗雷德·亚瑟事工（G. W. Arthur Ministries）的牧者和推动力量。亚瑟博士对上帝话语的恢复深感承诺，他为事工带来了激情、智慧和爱的独特融合。",
+        footerInfo: "George Wilfred Arthur 是乔治·威尔弗雷德·亚瑟事工（G. W. Arthur Ministries）的牧者和推动力的核心。对上帝道的深刻承诺，使得乔治·威尔弗雷德·亚瑟牧师为他的事工带来了独特的激情、智慧和爱。",
         footerLinks: "链接",
-        footerHome: "主页",
+        footerHome: "首页",
         footerBiography: "传记",
         footerContact: "联系",
         footerExternalLinks: "外部链接",
-        footerGallery: "来自画廊",
-        footerCopyright: "版权所有 © G W Arthur Ministries - 2023"
-    }
+        footerGallery: "从图库",
+        footerCopyright: "版权所有 @G. W. Arthur Ministries - 2024"
+    },
 }
 
 export default footerText
