ad6a90aa59585e2b0163bdb9977feb493978d583
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2024-03-04 00:58:27 +0000

    some local changes

--- src/internationalization/contact.ts
+++ src/internationalization/contact.ts
@@ -1,58 +1,58 @@
 const contactText = {
     en: {
         contactHeroText: "Get In Touch",
-        contactCall: "Call Me",
-        contactEmail: "Email Me",
+        contactCall: "Call",
+        contactEmail: "Email",
 
-        questionsTitle: "Ask G. W. Arthur",
+        questionsTitle: "Ask Dr. G. W. Arthur",
         questionsSubtext: "We've Got the Answers! Fire away using the form below. Whether it's a prayer request or just a friendly shout-out, we're here for you! Your connection is what makes the journey exciting.",
         questionsNameInput: "What is your name",
         questionsQuestionInput: "What question is on your heart",
         questionsButton: "Ask Now",
     },
     es: {
-        contactHeroText: "Ponte en contacto",
-        contactCall: "Llámame",
-        contactEmail: "Envíame un correo",
+        contactHeroText: "Ponte en Contacto",
+        contactCall: "Llamar",
+        contactEmail: "Correo Electrónico",
 
-        questionsTitle: "Pregunta al G. W. Arthur",
-        questionsSubtext: "¡Tenemos las respuestas! Haz tu pregunta utilizando el formulario a continuación. Ya sea una solicitud de oración o simplemente un saludo amistoso, ¡estamos aquí para ti! Tu conexión es lo que hace emocionante el viaje.",
+        questionsTitle: "Pregunta al Dr. G. W. Arthur",
+        questionsSubtext: "¡Tenemos las respuestas! Haz tus preguntas usando el formulario a continuación. Ya sea una solicitud de oración o simplemente un saludo amistoso, ¡estamos aquí para ti! Tu conexión es lo que hace emocionante el viaje.",
         questionsNameInput: "¿Cuál es tu nombre",
         questionsQuestionInput: "¿Qué pregunta tienes en tu corazón",
-        questionsButton: "Preguntar ahora",
+        questionsButton: "Preguntar ahora"
     },
     pt: {
         contactHeroText: "Entre em Contato",
-        contactCall: "Me Ligue",
-        contactEmail: "Me Envie um E-mail",
+        contactCall: "Ligar",
+        contactEmail: "E-mail",
 
-        questionsTitle: "Pergunte ao G. W. Arthur",
-        questionsSubtext: "Temos as respostas! Envie sua pergunta usando o formulário abaixo. Seja uma solicitação de oração ou apenas um cumprimento amigável, estamos aqui para você! Sua conexão é o que torna a jornada emocionante.",
+        questionsTitle: "Pergunte ao Dr. G. W. Arthur",
+        questionsSubtext: "Nós temos as respostas! Faça suas perguntas usando o formulário abaixo. Seja um pedido de oração ou apenas um cumprimento amigável, estamos aqui para você! Sua conexão é o que torna a jornada emocionante.",
         questionsNameInput: "Qual é o seu nome",
         questionsQuestionInput: "Qual pergunta está em seu coração",
-        questionsButton: "Perguntar Agora",
+        questionsButton: "Pergunte Agora"
     },
     fr: {
-        contactHeroText: "Prenez contact",
-        contactCall: "Appelez-moi",
-        contactEmail: "Envoyez-moi un e-mail",
+        contactHeroText: "Entrer en Contact",
+        contactCall: "Appeler",
+        contactEmail: "E-mail",
 
-        questionsTitle: "Demandez au G. W. Arthur",
-        questionsSubtext: "Nous avons les réponses ! Posez votre question en utilisant le formulaire ci-dessous. Que ce soit une demande de prière ou simplement un salut amical, nous sommes là pour vous ! Votre connexion est ce qui rend le voyage excitant.",
+        questionsTitle: "Posez votre question au Dr G. W. Arthur",
+        questionsSubtext: "Nous avons les réponses ! Posez vos questions en utilisant le formulaire ci-dessous. Que ce soit une demande de prière ou simplement un salut amical, nous sommes là pour vous ! Votre connexion est ce qui rend le voyage excitant.",
         questionsNameInput: "Quel est votre nom",
-        questionsQuestionInput: "Quelle question avez-vous sur le cœur",
-        questionsButton: "Demander maintenant",
+        questionsQuestionInput: "Quelle question avez-vous dans votre cœur",
+        questionsButton: "Demander maintenant"
     },
     ch: {
-        contactHeroText: "联系我们",
-        contactCall: "给我打电话",
-        contactEmail: "给我发邮件",
+        contactHeroText: "取得联系",
+        contactCall: "致电",
+        contactEmail: "电子邮件",
 
-        questionsTitle: "询问 G. W.亚瑟",
-        questionsSubtext: "我们有答案！通过下面的表格提问。无论是祷告请求还是友好的问候，我们都在这里为您服务！您的连接是使旅程变得令人兴奋的原因。",
+        questionsTitle: "问G. W. Arthur博士",
+        questionsSubtext: "我们有答案！使用下面的表格提出问题。无论是祈祷请求还是友好的问候，我们都在这里等着你！你的连接是使旅程变得令人兴奋的关键。",
         questionsNameInput: "你叫什么名字",
         questionsQuestionInput: "你心中有什么问题",
-        questionsButton: "现在问",
+        questionsButton: "现在问"
     },
 };
 
--- src/internationalization/footer.ts
+++ src/internationalization/footer.ts
@@ -1,61 +1,58 @@
 const footerText = {
     en: {
         footerTitle: "About Dr. G. W. Arthur",
-        footerInfo: "Dr. George Wilfred Arthur is the shepherd and driving force behind George Wilfred Arthur Ministries (G. W. Arthur Ministries). With a profound commitment to restoring the integrity of the Word of God, Rev. George brings a unique blend of passion, wisdom, and love to his ministry.",
+        footerInfo: "Dr. George Wilfred Arthur is the shepherd and driving force behind George Wilfred Arthur Ministries (G. W. Arthur Ministries). With a profound commitment to restoring the integrity of the Word of God, Dr. G. W. Arthur brings a unique blend of passion, wisdom, and love to his ministry.",
         footerLinks: "Links",
         footerHome: "Home",
         footerBiography: "Biography",
         footerContact: "Contact",
         footerExternalLinks: "External Links",
         footerGallery: "From The Gallery",
-        footerCopyright: "Copyright @G. W. Arthur Ministries - 2024"
+        footerCopyright: "Copyright @ G. W. Arthur Ministries - 2024"
     },
     fr: {
-        footerTitle: "À propos du Dr. G. W. Arthur",
-        footerInfo: "Dr. George Wilfred Arthur est le berger et la force motrice derrière les Ministères George Wilfred Arthur (G. W. Arthur Ministries). Avec un engagement profond à restaurer l'intégrité de la Parole de Dieu, le Révérend George apporte un mélange unique de passion, de sagesse et d'amour à son ministère",
+        footerTitle: "À propos du Dr G. W. Arthur",
+        footerInfo: "Le Dr George Wilfred Arthur est le berger et la force motrice derrière le ministère George Wilfred Arthur (G. W. Arthur Ministries). Avec un engagement profond à restaurer l'intégrité de la Parole de Dieu, le Dr G. W. Arthur apporte à son ministère une combinaison unique de passion, de sagesse et d'amour.",
         footerLinks: "Liens",
         footerHome: "Accueil",
         footerBiography: "Biographie",
         footerContact: "Contact",
         footerExternalLinks: "Liens Externes",
         footerGallery: "De la Galerie",
-        footerCopyright: "Droits d'auteur @G. W. Arthur Ministries - 2024"
+        footerCopyright: "Droits d'auteur @ G. W. Arthur Ministries - 2024"
     },
-
     pt: {
         footerTitle: "Sobre o Dr. G. W. Arthur",
-        footerInfo: "O Dr. George Wilfred Arthur é o pastor e a força motriz por trás dos Ministérios George Wilfred Arthur (Ministérios G. W. Arthur). Com um compromisso profundo de restaurar a integridade da Palavra de Deus, o Rev. George traz uma mistura única de paixão, sabedoria e amor para seu ministério.",
+        footerInfo: "O Dr. George Wilfred Arthur é o pastor e a força motriz por trás dos Ministérios George Wilfred Arthur (G. W. Arthur Ministries). Com um compromisso profundo de restaurar a integridade da Palavra de Deus, o Dr. G. W. Arthur traz uma combinação única de paixão, sabedoria e amor para o seu ministério.",
         footerLinks: "Links",
         footerHome: "Início",
         footerBiography: "Biografia",
         footerContact: "Contato",
         footerExternalLinks: "Links Externos",
         footerGallery: "Da Galeria",
-        footerCopyright: "Direitos Autorais @G. W. Arthur Ministries - 2024"
+        footerCopyright: "Direitos Autorais @ G. W. Arthur Ministries - 2024"
     },
-
     es: {
         footerTitle: "Acerca del Dr. G. W. Arthur",
-        footerInfo: "George Wilfred Arthur es el pastor y la fuerza impulsora detrás de los Ministerios George Wilfred Arthur (G. W. Arthur Ministries). Con un compromiso profundo con la restauración de la Palabra de Dios, el Rev. George aporta una mezcla única de pasión, sabiduría y amor a su ministerio.",
+        footerInfo: "El Dr. George Wilfred Arthur es el pastor y la fuerza impulsora detrás de los Ministerios George Wilfred Arthur (G. W. Arthur Ministries). Con un compromiso profundo de restaurar la integridad de la Palabra de Dios, el Dr. G. W. Arthur aporta a su ministerio una combinación única de pasión, sabiduría y amor.",
         footerLinks: "Enlaces",
         footerHome: "Inicio",
         footerBiography: "Biografía",
         footerContact: "Contacto",
         footerExternalLinks: "Enlaces Externos",
         footerGallery: "Desde la Galería",
-        footerCopyright: "Derechos de autor @G. W. Arthur Ministries - 2024"
+        footerCopyright: "Derechos de autor @ G. W. Arthur Ministries - 2024"
     },
-
     ch: {
-        footerTitle: "关于亚瑟博士",
-        footerInfo: "乔治·威尔弗雷德·阿瑟博士是乔治·威尔弗雷德·阿瑟事工（G. W. Arthur Ministries）的牧师和推动力量。凭借对上帝话语的恢复性诚意，乔治牧师将激情、智慧和爱结合到他的事工中。",
+        footerTitle: "关于G. W. Arthur博士",
+        footerInfo: "George Wilfred Arthur博士是George Wilfred Arthur Ministries（G. W. Arthur Ministries）的牧者和推动力。在深刻承诺恢复上帝之道的完整性方面，G. W. Arthur博士将激情、智慧和爱结合在他的事工中。",
         footerLinks: "链接",
         footerHome: "首页",
         footerBiography: "传记",
         footerContact: "联系",
         footerExternalLinks: "外部链接",
-        footerGallery: "从图库",
-        footerCopyright: "版权所有 @G. W. Arthur Ministries - 2024"
+        footerGallery: "来自画廊",
+        footerCopyright: "版权所有 @ G. W. Arthur Ministries - 2024"
     },
 }
 
